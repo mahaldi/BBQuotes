dc257f408579291b24afdbbca849e0d6509a6d55
Mahaldi WIjaya Saputra <aldiwijayasaputra@gmail.com>
2026-01-02 21:02:23 +0700

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by ceboi on 02/01/26.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
